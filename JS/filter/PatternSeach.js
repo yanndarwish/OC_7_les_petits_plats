ace81e3fe1c4f5b6c642979b9f373b82d5301b0e
Yann Darwish <yann.darwish@gmail.com>
2022-08-09 20:21:23 +0200

    add space in string

--- JS/filter/PatternSeach.js
+++ JS/filter/PatternSeach.js
@@ -8,13 +8,13 @@ class PatternSearch {
         Recipes.forEach(recipe => {
             let string = ''
             // add title to string
-            string += recipe.name.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "")
+            string += ' ' + recipe.name.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "")
             // add ingredients to string
             recipe.ingredients.forEach(ingredient => {
                 string += ' ' + ingredient.ingredient.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "")
             })
             // add decription to string
-            string += recipe.description.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "")
+            string += ' ' + recipe.description.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "")
             // compare the value with the new string to check for matches
             // if match, push in filteredRecipes
             if (PatternSearch.search(string, value)) {
