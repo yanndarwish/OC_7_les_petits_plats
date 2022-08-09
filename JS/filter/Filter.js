0020ab449b6d5ddba03e2fa71fb5b87be1eb7ad0
Yann Darwish <yann.darwish@gmail.com>
2022-08-09 14:02:48 +0200

    clean filter file

--- JS/filter/Filter.js
+++ JS/filter/Filter.js
@@ -6,42 +6,9 @@
 
 class Filter {
     constructor(Recipes) {
-        // this.Recipes         =  Recipes
-        // this.OriginalRecipes = Recipes
-        // this.hasTags         = false
-        // this.$container      = document.querySelector('.main-container')
     }
 
-    // filter(Recipes, input, option) {
-    //     // this.Tags = new Tags()
-    //     // this.PatternSearch = new PatternSearch()
-    //     // value is a string, send to patternSearch
-    //     if (typeof input === 'string') {
-    //         console.log(this.hasTags)
-    //         this.Recipes = this.PatternSearch.format(this.hasTags ? this.Recipes : Recipes, input)
-    //         console.log(this.Recipes)
-
-    //         this.displayRecipes(this.Recipes)
-
-    //     } else if (typeof input === 'object'){
-    //         console.log('from Tags')
-    //         input.length > 0 ? this.hasTags = true : this.hasTags = false
-    //         console.log(this.hasTags)
-    //             // this.Tags = new Tags(Recipes)
-    //             // option === 'back' 
-    //             //     ? .filterByTags(this.OriginalRecipes, input)
-    //             //     : .filterByTags(this.Recipes, input)
-    //             this.Recipes = this.Tags.format(this.Recipes, input)
-    //             this.displayRecipes(this.Recipes)
-
-    //     } else {
-    //         // this.Tags.format(this.Recipes)
-    //     }
-    // } 
-
     static displayRecipes(Recipes) {
-        // this.Recipes = Recipes
-
         document.querySelector('.main-container').innerHTML = ""
 
         if (Recipes.length === 0) {
